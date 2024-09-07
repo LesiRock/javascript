3fdee0de4dffec349d1c3a48479c1d8e5f3b4568
Isel Rebolledo <127368786+LesiRock@users.noreply.github.com>
2024-09-07 17:44:10 -0600

    Add files via upload
    Con dias

---
+++ PromedioIsel/PromedioIsel/js/main.js
@@ -0,0 +1,61 @@
+var fecha = new Date();
+
+var captura = confirm("Quienes capturar un alumno");
+while(captura == true){
+    var nombre = prompt("Escribe el nombre del alumno");
+    var calificacion = 0;
+    var sumaCalificaciones1 = 0;
+    var sumaCalificaciones2 = 0;
+    var sumaCalificaciones3 = 0;
+
+    for(var i = 0;i < 3;i++){
+        calificacion = parseFloat(prompt(`Escribe la calificacion del periodo 1: ${i+1}`));
+        sumaCalificaciones1 = sumaCalificaciones1 + calificacion;
+    }
+        var promedioPeriodo1 = sumaCalificaciones1 / 3;
+        alert(`El promedio del periodo 1 es: ${promedioPeriodo1}`);
+
+        for(var i = 0;i < 3;i++){
+            calificacion = parseFloat(prompt(`Escribe la calificacion del periodo 2: ${i+1}`));
+            sumaCalificaciones2 = sumaCalificaciones2 + calificacion;
+        }
+        var promedioPeriodo2 = sumaCalificaciones2 / 3;
+        alert(`El promedio del periodo 2 es: ${promedioPeriodo2}`);
+
+        for(var i = 0;i < 3;i++){
+            calificacion = parseFloat(prompt(`Escribe la calificacion del periodo 3: ${i+1}`));
+            sumaCalificaciones3 = sumaCalificaciones3 + calificacion;
+        }
+        var promedioPeriodo3 = sumaCalificaciones3 / 3;
+        alert(`El promedio del periodo 3 es: ${promedioPeriodo3}`);
+
+    var promedioGeneral = (promedioPeriodo1 +promedioPeriodo2 +promedioPeriodo3)/3;
+    alert(`El nombre del alumno: ${nombre} \npromedio general es: ${promedioGeneral}`);
+
+
+    switch(true){
+        case promedioGeneral < 6:
+            alert("REPROBADO");
+            break;
+        case promedioGeneral < 8:
+            alert("BIEN");
+            break;
+        case promedioGeneral < 10:
+            alert("MUY BIEN");
+            break;
+        case promedioGeneral == 10:
+            alert("EXCELENTE");
+            break;
+        default:
+            alert("Promedio Invalido");
+    }
+
+    var dia = fecha.getDate();
+    var mes = fecha.getMonth();
+    var anio = fecha.getFullYear();
+    alert(`La fecha de hoy es:\nEl día ${dia} del mes ${mes} del año ${anio}`);
+    var captura = confirm("Quienes capturar un alumno");
+
+}
+alert("HASTA LUEGO 😥");
+
